581bc7b4a9bab48c786f519268bf188cd056e56d
XamuelUG <samweko@gmail.com>
2022-11-20 07:20:52 +0300

    added 404

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.get("/", (req, res) => {
 conn();
 
 /**
+ *
+ * 404
+ */
+app.use((req, res, next) => {
+  res.status(404);
+  res.send("404");
+});
+
+/**
  * Server
  */
 app.listen(PORT, () => {
